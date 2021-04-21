39c66cc80ee1b73445f32f9af3a6aabd52818c34
Ashfaq <tricksfree.in@gmail.com>
2021-04-21 16:52:08 +0530

    Added the Debug

--- features/signup/singupSlice.js
+++ features/signup/singupSlice.js
@@ -1,47 +1,15 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk  } from '@reduxjs/toolkit'
 import axios from 'axios';
 // import Api from '../API/api';
 
 const singupSlice = createSlice({
   name: 'singup',
-  initialState : {errors: []},
-  reducers: {
-    registerUser :  async (state, action) => {
-      // state.errors = {name : 'Hello'};
-      console.log(state.errors);
-      try {
-        // fetch data from a url endpoint
-        const response = await axios.post(`http://192.168.0.108/api/v1/register`,{ ...action.payload});
-        const data = await response.json();
-        console.log('====================================');
-        console.log(data);
-        console.log('====================================');
+  initialState : {
+    status: 'idle',
+    error: null},
+    reducers: {
+    registerUser :   (state, action) => {
   
-      } catch (error) {
-        if (error.response.status === 422) {
-          state.errors.push(error.response.data.errors);
-        }
-        // console.log(error.response.status);
-        // console.log(error.response.data.errors); // catches both errors
-      }
-
-      // console.log(action.payload);
-    
-
-    //  axios.post(`http://192.168.0.108/api/v1/register`,{
-    //      ...action.payload
-    //  })
-    //  .then(success => console.log(success))
-    //  .catch(error => {
-    //   // if (error.response.status === 422) {
-  
-        
-    //     // console.log(state);
-        
-    //   // }
-    //  });
-
-
     },
   }
 })
