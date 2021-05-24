7d1750d038586e0107e6125e8405261c35b793ea
unknown <vijaydivakar6@gmail.com>
2021-05-24 19:30:41 +0530

    working on payement part

---
+++ screens/Payment.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  Alert,
+  Button,
+  Platform,
+  View,
+  Image,
+} from "react-native";
+
+import IAP from "react-native-iap";
+
+const Payment = () => {
+  const [purchased, setPurchased] = useState(false); //set to true if the user has active subscription
+  const [products, setProducts] = useState({}); //used to store list of products
+   
+  useEffect(() => {
+    IAP.initConnection()
+      .catch(() => {
+        console.log("error connecting to store...");
+      })
+      .then(() => {
+        IAP.getSubscriptions(items)
+          .catch(() => {
+            console.log("error finding items");
+          })
+        })
+          .then((res) => {
+            console.log("hey");
+          })
+      }, []);
+  
+  return (
+    <View>
+      <Text>Hai</Text>
+    </View>
+  )
+
+}
+
+export default Payment;
