101b7864f6806f637b6dc82038eb774e44a241a3
unknown <vijaydivakar6@gmail.com>
2021-05-28 17:05:24 +0530

    splash dcreen fixed

--- App.js
+++ App.js
@@ -10,14 +10,18 @@ import { Dashboard }from './screens'
 import { Provider } from 'react-redux'
 import store from './store/store';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
+import LottieView from 'lottie-react-native';
+import Loader from './components/Loader';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
+
 const  App =({navigation}) =>{
+
   return (
     <Provider store={store}>
+        <Loader/>
          <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
